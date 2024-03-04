ec4fa4b1d8793856ded16370e5f1270e7bbbed7c
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-03-04 21:48:13 +0530

    implementing the pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Container, Paper, TextField, Typography } from "@mui/material";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [isLogin, setisLogin] = useState(true);
+  const ToggleLogin = () => setisLogin(false);
+  return (
+    <Container
+      component={"main"}
+      maxWidth="xs"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {isLogin ? (
+          <>
+            <Typography variant="h4">Login</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+            >
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="username"
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="password"
+                type="password"
+              />
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                Login
+              </Button>
+              <Typography textAlign={"center"} m={"1rem"}>
+                OR
+              </Typography>
+              <Button
+                variant="text"
+                type="submit"
+                onClick={ToggleLogin}
+                fullWidth
+              >
+                SignUp Instead
+              </Button>
+            </form>
+          </>
+        ) : (
+          <span> Register</span>
+        )}
+      </Paper>
+    </Container>
+  );
 };
 
 export default Login;
