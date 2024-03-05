3098b4846123c9a7e7ab7d54266b45e0d2e17b32
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-03-05 21:14:35 +0530

    login and registration completed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
-import { Button, Container, Paper, TextField, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { CameraAlt as CameraAltIcon } from "@mui/icons-material";
+import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
+import { useFileHandler, useInputValidation, useStrongPassword } from "6pp";
+import { usernameValidator } from "../utils/validators";
 
 const Login = () => {
   const [isLogin, setisLogin] = useState(true);
-  const ToggleLogin = () => setisLogin(false);
+  const ToggleLogin = () => setisLogin((prev) => !prev);
+  const name = useInputValidation("");
+  const bio = useInputValidation("");
+  const username = useInputValidation("", usernameValidator);
+  const password = useStrongPassword("");
+  const avatar = useFileHandler("single", 2);
+  const handleLogin=(e)=>{
+    e.preventDefault();
+  }
+  const handleregister=(e)=>{
+    e.preventDefault();
+  }
+
   return (
     <Container
       component={"main"}
@@ -32,6 +57,7 @@ const Login = () => {
                 width: "100%",
                 marginTop: "1rem",
               }}
+              onSubmit={handleLogin}
             >
               <TextField
                 variant="outlined"
@@ -39,6 +65,8 @@ const Login = () => {
                 required
                 fullWidth
                 label="username"
+                value={username.value}
+                onChange={username.changeHandler}
               />
               <TextField
                 variant="outlined"
@@ -47,6 +75,8 @@ const Login = () => {
                 fullWidth
                 label="password"
                 type="password"
+                value={password.value}
+                onChange={password.changeHandler}
               />
               <Button
                 sx={{
@@ -73,7 +103,130 @@ const Login = () => {
             </form>
           </>
         ) : (
-          <span> Register</span>
+          <>
+            <Typography variant="h4">Register</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+              onSubmit={handleregister}
+            >
+              <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+                <Avatar
+                  sx={{
+                    objectFit: "contain",
+                    height: "10rem",
+                    width: "10rem",
+                  }}
+                  src={avatar.preview}
+                />
+
+                <IconButton
+                  sx={{
+                    position: "absolute",
+                    bottom: 0,
+                    right: 0,
+                    color: "white",
+                    bgcolor: "rgb(0,0,0,0.5)",
+                    ":hover": {
+                      bgcolor: "rgb(0,0,0,0.7)",
+                    },
+                  }}
+                  component="label"
+                >
+                  <>
+                    <CameraAltIcon />
+                    <VisuallyHiddenInput
+                      type="file"
+                      onChange={avatar.changeHandler}
+                    />
+                  </>
+                </IconButton>
+              </Stack>
+              {avatar.error && (
+                <Typography
+                  m={"1rem auto"}
+                  color="error"
+                  width={"fit-content"}
+                  display={"block"}
+                  variant="caption"
+                >
+                  {avatar.error}
+                </Typography>
+              )}
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Name"
+                value={name.value}
+                onChange={name.changeHandler}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="username"
+                value={username.value}
+                onChange={username.changeHandler}
+              />
+              {username.error && (
+                <Typography color="error" variant="caption">
+                  {username.error}
+                </Typography>
+              )}
+
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Bio"
+                value={bio.value}
+                onChange={bio.changeHandler}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="password"
+                type="password"
+                value={password.value}
+                onChange={password.changeHandler}
+              />
+              {password.error && (
+                <Typography color="error" variant="caption">
+                  {password.error}
+                </Typography>
+              )}
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                SignUp
+              </Button>
+              <Typography textAlign={"center"} m={"1rem"}>
+                OR
+              </Typography>
+              <Button
+                variant="text"
+                type="submit"
+                onClick={ToggleLogin}
+                fullWidth
+              >
+                Login
+              </Button>
+            </form>
+          </>
         )}
       </Paper>
     </Container>
