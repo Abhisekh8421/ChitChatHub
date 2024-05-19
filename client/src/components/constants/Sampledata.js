b5a197ea00c8fd12b1742ae24a12db327cafce4d
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-05-19 22:46:06 +0530

    implementing the Admin Dashboard

--- client/src/components/constants/Sampledata.js
+++ client/src/components/constants/Sampledata.js
@@ -84,7 +84,7 @@ export const sampleMessage = [
     attachments: [
       {
         public_id: "attachment1_public_id",
-        url: "C:\Users\Abhisekh\Downloads\sample-boat-400x300.png",
+        url: "C:UsersAbhisekhDownloadssample-boat-400x300.png",
       },
     ],
     content: "This is the first message content.",
@@ -100,7 +100,7 @@ export const sampleMessage = [
     attachments: [
       {
         public_id: "attachment2_public_id",
-        url: "C:\Users\Abhisekh\Downloads\sample-boat-400x300.png",
+        url: "C:UsersAbhisekhDownloadssample-boat-400x300.png",
       },
     ],
     content: "This is the second message content.",
@@ -129,3 +129,92 @@ export const sampleMessage = [
     createdAt: "2024-03-21T12:05:00Z",
   },
 ];
+
+export const dashboardData = {
+  users: [
+    {
+      name: "John Doe",
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      _id: "1",
+      username: "john_doe",
+      friends: 20,
+      groups: 5,
+    },
+    {
+      name: "John Boi",
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      _id: "2",
+      username: "john_boi",
+      friends: 20,
+      groups: 25,
+    },
+  ],
+
+  chats: [
+    {
+      name: "LabadBass Group",
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+      _id: "1",
+      groupChat: false,
+      members: [
+        { _id: "1", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "2", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+      ],
+      totalMembers: 2,
+      totalMessages: 20,
+      creator: {
+        name: "John Doe",
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      },
+    },
+    {
+      name: "L*Da Luston Group",
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+      _id: "2",
+      groupChat: true,
+      members: [
+        { _id: "1", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "2", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+      ],
+      totalMembers: 2,
+      totalMessages: 20,
+      creator: {
+        name: "John Boi",
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      },
+    },
+  ],
+
+  messages: [
+    {
+      attachments: [],
+      content: "L*uda ka Message hai",
+      _id: "sfnsdjkfsdnfkjsbnd",
+      sender: {
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+        name: "Chaman ",
+      },
+      chat: "chatId",
+      groupChat: false,
+      createdAt: "2024-02-12T10:41:30.630Z",
+    },
+
+    {
+      attachments: [
+        {
+          public_id: "asdsad 2",
+          url: "https://www.w3schools.com/howto/img_avatar.png",
+        },
+      ],
+      content: "",
+      _id: "sfnsdjkfsdnfkdddjsbnd",
+      sender: {
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+        name: "Chaman  2",
+      },
+      chat: "chatId",
+      groupChat: true,
+      createdAt: "2024-02-12T10:41:30.630Z",
+    },
+  ],
+};
--- client/src/components/layout/AdminLayout.jsx
+++ client/src/components/layout/AdminLayout.jsx
@@ -1,13 +1,123 @@
-import { Grid } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Drawer,
+  Grid,
+  IconButton,
+  Stack,
+  Typography,
+  styled,
+} from "@mui/material";
+import React, { useState } from "react";
+import {
+  Close,
+  Dashboard,
+  ExitToApp,
+  Groups,
+  ManageAccounts,
+  Menu,
+  Message,
+} from "@mui/icons-material";
+import { Link as LinkComponent, Navigate, useLocation } from "react-router-dom";
+const Link = styled(LinkComponent)`
+  text-decoration: none;
+  border-radius: 2rem;
+  padding: 1rem 2rem;
+  color: black;
+  &:hover {
+    color: rgba(0, 0, 0, 0.54);
+  }
+`;
 
-const Sidebar = () => {
-  return <div> Sidebar</div>;
+//tabs
+
+const adminTabs = [
+  {
+    name: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <Dashboard />,
+  },
+  {
+    name: "Users",
+    path: "/admin/users",
+    icon: <ManageAccounts />,
+  },
+  {
+    name: "Chats",
+    path: "/admin/chats",
+    icon: <Groups />,
+  },
+  {
+    name: "Messages",
+    path: "/admin/messages",
+    icon: <Message />,
+  },
+];
+
+const Sidebar = ({ w = "100%" }) => {
+  const location = useLocation();
+  const logoutHandler = () => {
+    console.log("logout");
+  };
+  return (
+    <Stack width={w} direction={"column"} p={"3rem"} spacing={"3rem"}>
+      <Typography variant="h4" textTransform={"uppercase"} textAlign={"center"}>
+        Admin
+      </Typography>
+      <Stack spacing={"1rem"}>
+        {adminTabs.map((tab) => (
+          <Link
+            to={tab.path}
+            key={tab.path}
+            sx={
+              location.pathname === tab.path && {
+                bgcolor: "#28282B",
+                color: "white",
+                ":hover": { color: "white" },
+              }
+            }
+          >
+            <Stack direction={"row"} alignItems={"center"} spacing={"1rem"}>
+              {tab.icon}
+              <Typography fontSize={"1.2rem"} fontWeight={"550"} fontFamily={"sans-serif"}>
+                {tab.name}
+              </Typography>
+            </Stack>
+          </Link>
+        ))}
+        <Link onClick={logoutHandler}>
+          <Stack direction={"row"} alignItems={"center"} spacing={"1rem"}>
+            <ExitToApp />
+            <Typography fontSize={"1.2rem"} fontWeight={"550"} fontFamily={"sans-serif"}>
+              Logout
+            </Typography>
+          </Stack>
+        </Link>
+      </Stack>
+    </Stack>
+  );
 };
-
+const isAdmin = true;
 const AdminLayout = ({ children }) => {
+  const [isMobile, setIsmobile] = useState(false);
+  const handleMobile = () => setIsmobile((prev) => !prev);
+  const handleClose = () => setIsmobile(false);
+  if (!isAdmin) return <Navigate to="/admin" />;
   return (
     <Grid container minHeight={"100vh"}>
+      <Box
+        sx={{
+          display: { xs: "block", md: "none" },
+          position: "fixed",
+          right: "1rem",
+          top: "1rem",
+        }}
+      >
+        <IconButton onClick={handleMobile}>
+          {isMobile ? <Close /> : <Menu />}
+        </IconButton>
+      </Box>
+      {/* mainly this box will appear only if the screen size will be xs and not visible at md it is like a menu icon */}
+
       <Grid
         item
         md={4}
@@ -32,6 +142,9 @@ const AdminLayout = ({ children }) => {
       >
         {children}
       </Grid>
+      <Drawer open={isMobile} onClose={handleClose}>
+        <Sidebar w="50vw" />
+      </Drawer>
     </Grid>
   );
 };
